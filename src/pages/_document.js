9bd49c86cf7288934dbbf994cfcd35b2016be50e
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-03-22 18:34:46 +0600

    adding initial project

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,11 +3,29 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <title>VTH Admin Dashboard</title>
+      <link id="theme-style" rel="stylesheet" href="/assets/css/portal.css"></link>
+      </Head>
+      <body className="app">
+        <div className="app-wrapper">
         <Main />
+        </div>
         <NextScript />
       </body>
+      <script defer src="/assets/plugins/fontawesome/js/all.min.js"></script>
+      {/* <script src="/assets/plugins/popper.min.js"></script> */}
+    <script src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script>  
+
+    
+    {/* <script src="/assets/plugins/chart.js/chart.min.js"></script> 
+    <script src="/assets/js/index-charts.js"></script>  */}
+    
+    
+    <script src="/assets/js/app.js"></script> 
+    
+    
+    
     </Html>
   );
 }
