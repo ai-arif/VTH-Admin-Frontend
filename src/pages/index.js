5364cdf43c92d320c64e9d5347bc3de45b7ff10c
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-03-30 23:52:40 +0600

    Commented out getServerSideProps function in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import UsersHome from '../../Components/Users/UsersHome'
 
-export const getServerSideProps = async (context) => {
-  const token = context.req.cookies.token
+// export const getServerSideProps = async (context) => {
+//   const token = context.req.cookies.token
   
-  if (!token || token === 'null' || token==='undefined') {
-    return {
-      redirect: {
-        destination: '/auth/login',
-        permanent: false,
-      },
-    }
-  }
+//   if (!token || token === 'null' || token==='undefined') {
+//     return {
+//       redirect: {
+//         destination: '/auth/login',
+//         permanent: false,
+//       },
+//     }
+//   }
   
-  return {
-    props: { token },
-  }
-}
+//   return {
+//     props: { token },
+//   }
+// }
 
 
 const index = () => {
