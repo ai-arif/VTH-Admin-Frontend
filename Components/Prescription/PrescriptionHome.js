6dc6cfcd0613b5bf7e8c75c4598c1d1369a99d7d
atikur-24 <atikurrahman7223@gmail.com>
2024-05-02 22:44:30 +0600

    apply dark table

--- Components/Prescription/PrescriptionHome.js
+++ Components/Prescription/PrescriptionHome.js
@@ -1,120 +1,119 @@
-import React from 'react'
+import React from "react";
 
 const PrescriptionHome = () => {
-    return (
-        <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-12'>
-                    <div className='card'>
-                        <div className='card-header'>
-                            <h4 className='card-header-title'>Prescription</h4>
-                        </div>
-                        <div className='card-body'>
-                            <div className="row mb-4">
-                            <div className="col-md-6">
-                                <input type="text" className="form-control" placeholder="Recipient's Phone" aria-label="Recipient's phone" aria-describedby="button-addon2" />
-                                <button className="btn my-2 mx-1 btn-primary text-white" type="button" id="button-addon2">Search</button> 
-                                <span className="text-secondary small">(First search appointment using owner's phone)</span>
-                            </div>
-                            <div className='col-md-6'></div>
-                            </div>
-                            <form>
-                            <div className="row">
-                                    <div className='mb-3 col-md-6'>
-                                        <label htmlFor='name' className='form-label'>
-                                            Select Appointment
-                                        </label>
-                                        {/* select field */}
-                                        <select className="form-select" aria-label="Default select example">
-                                            <option selected>Select</option>
-                                            <option value="1">One</option>
-                                            <option value="2">Two</option>
-                                            <option value="3">Three</option>
-                                        </select>
-                                    </div>
-                                    <div className='mb-3 col-md-6'>
-                                        <label htmlFor='phone' className='form-label'>
-                                            CASE NO
-                                        </label>
-                                        <input readOnly={true} type='text' className='form-control' value={"pxx3233Wr"} id='phone' />
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className='mb-3 col-md-6'>
-                                        <label htmlFor='name' className='form-label'>
-                                            Owner Name
-                                        </label>
-                                        <input type='text' className='form-control' id='name' />
-                                    </div>
-                                    <div className='mb-3 col-md-6'>
-                                        <label htmlFor='phone' className='form-label'>
-                                            Phone
-                                        </label>
-                                        <input type='text' className='form-control' id='phone' />
-                                    </div>
-                                </div>
-                                <div className="row">
-                                <div className='mb-3 col-md-6'>
-                                    <label htmlFor='email' className='form-label'>
-                                        District
-                                    </label>
-                                    <select className="form-select" aria-label="Default select example">
-                                        <option selected>Select</option>
-                                        <option value="1">Rajshahi</option>
-                                        <option value="2">Mymensingh</option>
-                                        <option value="3">Three</option>
-                                    </select>
-                                </div>
-                                <div className='mb-3 col-md-6'>
-                                    <label htmlFor='address' className='form-label'>
-                                        Upazila
-                                    </label>
-                                    {/* select dropdown for upazila */}
-                                    <select className="form-select" aria-label="Default select example">
-                                        <option selected>Select</option>
-                                        <option value="1">Chapainawabganj</option>
-                                        <option value="2">Sibganj</option>
-                                        <option value="3">Three</option>
-                                    </select>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="addres">Address</label>
-                                    <textarea className="form-control" id="addres" rows="3"></textarea>
-                                </div>
-                                <div className="row">
-                                <div className='mb-3 col-md-6'>
-                                    <label htmlFor='email' className='form-label'>
-                                        Medicine
-                                    </label>
-                                    <select className="form-select" aria-label="Default select example">
-                                        <option selected>Select</option>
-                                        <option value="1">Napa</option>
-                                        <option value="2">Alatrol</option>
-                                        <option value="3">Three</option>
-                                    </select>
-                                </div>
-                                    <div className='mb-3 col-md-6'>
-                                        <label htmlFor='phone' className='form-label'>
-                                            Diagnose
-                                        </label>
-                                        <input type='text' className='form-control' id='phone' />
-                                    </div>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="addres">Advice</label>
-                                    <textarea className="form-control" id="addres" rows="5"></textarea>
-                                </div>
-                                
-                                </div>
-                            </form>
-                        </div>
-
-                    </div>
-                </div>
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <div className="card">
+            <div className="card-header">
+              <h4 className="card-header-title">Prescription</h4>
             </div>
-
+            <div className="card-body">
+              <div className="row mb-4">
+                <div className="col-md-6">
+                  <input type="text" className="form-control" placeholder="Recipient's Phone" aria-label="Recipient's phone" aria-describedby="button-addon2" />
+                  <button className="btn my-2 mx-1 btn-primary text-white" type="button" id="button-addon2">
+                    Search
+                  </button>
+                  <span className="opacity-75 small">(First search appointment using owner's phone)</span>
+                </div>
+                <div className="col-md-6"></div>
+              </div>
+              <form>
+                <div className="row">
+                  <div className="mb-3 col-md-6">
+                    <label htmlFor="name" className="form-label">
+                      Select Appointment
+                    </label>
+                    {/* select field */}
+                    <select className="form-select" aria-label="Default select example">
+                      <option selected>Select</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                  <div className="mb-3 col-md-6">
+                    <label htmlFor="phone" className="form-label">
+                      CASE NO
+                    </label>
+                    <input readOnly={true} type="text" className="form-control" value={"pxx3233Wr"} id="phone" />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="mb-3 col-md-6">
+                    <label htmlFor="name" className="form-label">
+                      Owner Name
+                    </label>
+                    <input type="text" className="form-control" id="name" />
+                  </div>
+                  <div className="mb-3 col-md-6">
+                    <label htmlFor="phone" className="form-label">
+                      Phone
+                    </label>
+                    <input type="text" className="form-control" id="phone" />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="mb-3 col-md-6">
+                    <label htmlFor="email" className="form-label">
+                      District
+                    </label>
+                    <select className="form-select" aria-label="Default select example">
+                      <option selected>Select</option>
+                      <option value="1">Rajshahi</option>
+                      <option value="2">Mymensingh</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                  <div className="mb-3 col-md-6">
+                    <label htmlFor="address" className="form-label">
+                      Upazila
+                    </label>
+                    {/* select dropdown for upazila */}
+                    <select className="form-select" aria-label="Default select example">
+                      <option selected>Select</option>
+                      <option value="1">Chapainawabganj</option>
+                      <option value="2">Sibganj</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="addres">Address</label>
+                    <textarea className="form-control" id="addres" rows="3"></textarea>
+                  </div>
+                  <div className="row">
+                    <div className="mb-3 col-md-6">
+                      <label htmlFor="email" className="form-label">
+                        Medicine
+                      </label>
+                      <select className="form-select" aria-label="Default select example">
+                        <option selected>Select</option>
+                        <option value="1">Napa</option>
+                        <option value="2">Alatrol</option>
+                        <option value="3">Three</option>
+                      </select>
+                    </div>
+                    <div className="mb-3 col-md-6">
+                      <label htmlFor="phone" className="form-label">
+                        Diagnose
+                      </label>
+                      <input type="text" className="form-control" id="phone" />
+                    </div>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="addres">Advice</label>
+                    <textarea className="form-control" id="addres" rows="5"></textarea>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default PrescriptionHome
+export default PrescriptionHome;
--- Components/TestResult/TestResultHome.js
+++ Components/TestResult/TestResultHome.js
@@ -1,137 +1,138 @@
-import React from 'react'
+import React from "react";
 
 const TestResultHome = () => {
-    return (
-        <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-12'>
-                    <div className='card'>
-                        <div className='card-header'>
-                            <h4 className='card-header-title'>Test Result</h4>
-                        </div>
-                        <div className='card-body'>
-
-                            <form>
-                                <div className="row">
-                                    <div className='mb-3 col-md-6'>
-                                        <label htmlFor='phone' className='form-label'>
-                                            CASE NO
-                                        </label>
-                                        <input readOnly={true} type='text' className='form-control' value={"pxx3233Wr"} id='phone' />
-                                    </div>
-                                    <div className='mb-3 col-md-6'>
-                                        <label htmlFor='phone' className='form-label'>
-                                            Date:
-                                        </label>
-                                        <input type='text' className='form-control' placeholder={new Date().toDateString()} id='phone' />
-                                    </div>
-                                </div>
-                                <div className="d-flex justify-content-center my-3 mb-3">
-                                    <div>
-                                        <h4>Owner's Information</h4>
-                                        <hr />
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className='mb-3 col-md-6'>
-                                        <label htmlFor='name' className='form-label'>
-                                            Owner Name
-                                        </label>
-                                        <input type='text' className='form-control' id='name' />
-                                    </div>
-                                    <div className='mb-3 col-md-6'>
-                                        <label htmlFor='phone' className='form-label'>
-                                            Phone
-                                        </label>
-                                        <input type='text' className='form-control' id='phone' />
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className='mb-3 col-md-6'>
-                                        <label htmlFor='email' className='form-label'>
-                                            District
-                                        </label>
-                                        {/* select drop down for district */}
-                                        <select className="form-select" aria-label="Default select example">
-                                            <option selected>Select</option>
-                                            <option value="1">Rajshahi</option>
-                                            <option value="2">Mymensingh</option>
-                                            <option value="3">Three</option>
-                                        </select>
-                                    </div>
-                                    <div className='mb-3 col-md-6'>
-                                        <label htmlFor='address' className='form-label'>
-                                            Upazila
-                                        </label>
-                                        {/* select dropdown for upazila */}
-                                        <select className="form-select" aria-label="Default select example">
-                                            <option selected>Select</option>
-                                            <option value="1">Chapainawabganj</option>
-                                            <option value="2">Sibganj</option>
-                                            <option value="3">Three</option>
-                                        </select>
-                                    </div>
-                                    <div className="mb-3">
-                                        <label htmlFor="addres">Address</label>
-                                        <textarea className="form-control" id="addres" rows="3"></textarea>
-                                    </div>
-                                    <div className="d-flex justify-content-center my-3 mb-3">
-                                        <div>
-                                            <h4>Patient's Clinical Information</h4>
-                                            <hr />
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-md-12">
-                                            {/* create a table of clinical test results */}
-                                            <table className="table table-bordered">
-                                                <thead>
-                                                    <tr>
-                                                        <th scope="col">Test Name</th>
-                                                        <th scope="col">Result</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <td>Uterine lumen</td>
-                                                        <td>
-                                                            {/* checkbox of labels Visbile, Non-visible */}
-                                                            <div className="form-check form-check-inline">
-                                                                <input checked className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
-                                                                <label className="form-check-label" htmlFor="inlineCheckbox1">Visible</label>
-                                                            </div>
-                                                            <div className="form-check form-check-inline">
-                                                                <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
-                                                                <label className="form-check-label" htmlFor="inlineCheckbox2">Non-visible</label>
-                                                            </div>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Test 1</td>
-                                                        <td>Positive</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Test 2</td>
-                                                        <td>Negative</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Test 3</td>
-                                                        <td>Positive</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-
-                    </div>
-                </div>
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <div className="card">
+            <div className="card-header">
+              <h4 className="card-header-title">Test Result</h4>
             </div>
-
+            <div className="card-body">
+              <form>
+                <div className="row">
+                  <div className="mb-3 col-md-6">
+                    <label htmlFor="phone" className="form-label">
+                      CASE NO
+                    </label>
+                    <input readOnly={true} type="text" className="form-control" value={"pxx3233Wr"} id="phone" />
+                  </div>
+                  <div className="mb-3 col-md-6">
+                    <label htmlFor="phone" className="form-label">
+                      Date:
+                    </label>
+                    <input type="text" className="form-control" placeholder={new Date().toDateString()} id="phone" />
+                  </div>
+                </div>
+                <div className="d-flex justify-content-center my-3 mb-3">
+                  <div>
+                    <h4>Owner's Information</h4>
+                    <hr />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="mb-3 col-md-6">
+                    <label htmlFor="name" className="form-label">
+                      Owner Name
+                    </label>
+                    <input type="text" className="form-control" id="name" />
+                  </div>
+                  <div className="mb-3 col-md-6">
+                    <label htmlFor="phone" className="form-label">
+                      Phone
+                    </label>
+                    <input type="text" className="form-control" id="phone" />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="mb-3 col-md-6">
+                    <label htmlFor="email" className="form-label">
+                      District
+                    </label>
+                    {/* select drop down for district */}
+                    <select className="form-select" aria-label="Default select example">
+                      <option selected>Select</option>
+                      <option value="1">Rajshahi</option>
+                      <option value="2">Mymensingh</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                  <div className="mb-3 col-md-6">
+                    <label htmlFor="address" className="form-label">
+                      Upazila
+                    </label>
+                    {/* select dropdown for upazila */}
+                    <select className="form-select" aria-label="Default select example">
+                      <option selected>Select</option>
+                      <option value="1">Chapainawabganj</option>
+                      <option value="2">Sibganj</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="addres">Address</label>
+                    <textarea className="form-control" id="addres" rows="3"></textarea>
+                  </div>
+                  <div className="d-flex justify-content-center my-3 mb-3">
+                    <div>
+                      <h4>Patient's Clinical Information</h4>
+                      <hr />
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-12">
+                      {/* create a table of clinical test results */}
+                      <table className="table table-bordered table-dark">
+                        <thead>
+                          <tr>
+                            <th scope="col">Test Name</th>
+                            <th scope="col">Result</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>Uterine lumen</td>
+                            <td>
+                              {/* checkbox of labels Visbile, Non-visible */}
+                              <div className="form-check form-check-inline">
+                                <input checked className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
+                                <label className="form-check-label" htmlFor="inlineCheckbox1">
+                                  Visible
+                                </label>
+                              </div>
+                              <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
+                                <label className="form-check-label" htmlFor="inlineCheckbox2">
+                                  Non-visible
+                                </label>
+                              </div>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>Test 1</td>
+                            <td>Positive</td>
+                          </tr>
+                          <tr>
+                            <td>Test 2</td>
+                            <td>Negative</td>
+                          </tr>
+                          <tr>
+                            <td>Test 3</td>
+                            <td>Positive</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default TestResultHome
+export default TestResultHome;
