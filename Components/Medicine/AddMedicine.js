dd634c1a76c1f48f84360455be839520c56bf095
atikur-24 <atikurrahman7223@gmail.com>
2024-05-09 01:24:28 +0600

    post get and delete medicine data

--- Components/Medicine/AddMedicine.js
+++ Components/Medicine/AddMedicine.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { createMedicine } from "../../features/medicine/medicineSlice";
 
 const AddMedicine = () => {
+  const dispatch = useDispatch();
   const {
     handleSubmit,
     register,
@@ -9,15 +13,32 @@ const AddMedicine = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (medicine) => {
-    console.log(medicine);
+  const onSubmit = async (medicine) => {
+    try {
+      // Convert number type
+      medicine.price = parseFloat(medicine.price, 10);
+      medicine.unitPrice = parseFloat(medicine.unitPrice, 10);
+      medicine.quantity = parseInt(medicine.quantity, 10);
+
+      const response = await dispatch(createMedicine(medicine));
+
+      if (response?.payload?.success) {
+        toast.success("Medicine added successfully!");
+        reset();
+      } else {
+        toast.error("Failed to add medicine! Please try again later.");
+      }
+    } catch (error) {
+      console.error("An error occurred while adding medicine:", error);
+      toast.error("An error occurred while adding medicine. Please try again later.");
+    }
   };
 
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-12">
-          <div className="card">
+          <div className="card pb-4">
             <div className="card-header">
               <h4 className="card-header-title">Add Medicine</h4>
             </div>
@@ -42,18 +63,20 @@ const AddMedicine = () => {
                     <input readOnly={true} type="text" className="form-control" value={"pxx3233Wr"} id="phone" />
                   </div> */}
                 </div>
+
                 <div className="row">
                   <div className="mb-3 col-md-6">
-                    <label className="form-label">Brand Name</label>
-                    <input type="text" {...register("brand_name", { required: true })} className={`form-control ${errors.brand_name && "border-danger"}`} id="brand_name" />
-                    {errors.brand_name && <small className="text-danger">Please write brand name</small>}
+                    <label className="form-label">Medicine Name</label>
+                    <input type="text" {...register("name", { required: true })} className={`form-control ${errors.name && "border-danger"}`} id="name" />
+                    {errors.name && <small className="text-danger">Please write Medicine name</small>}
                   </div>
                   <div className="mb-3 col-md-6">
-                    <label className="form-label">Composition</label>
-                    <input type="text" {...register("composition", { required: true })} className={`form-control ${errors.composition && "border-danger"}`} id="composition" />
-                    {errors.composition && <small className="text-danger">Please write composition</small>}
+                    <label className="form-label">Brand Name</label>
+                    <input type="text" {...register("brandName", { required: true })} className={`form-control ${errors.brandName && "border-danger"}`} id="brandName" />
+                    {errors.brandName && <small className="text-danger">Please write brand name</small>}
                   </div>
                 </div>
+
                 <div className="row">
                   <div className="mb-3 col-md-6">
                     <label className="form-label">Class</label>
@@ -61,11 +84,12 @@ const AddMedicine = () => {
                     {errors.class && <small className="text-danger">Please write class</small>}
                   </div>
                   <div className="mb-3 col-md-6">
-                    <label className="form-label">Form</label>
-                    <input type="text" {...register("form", { required: true })} className={`form-control ${errors.form && "border-danger"}`} id="form" />
-                    {errors.form && <small className="text-danger">Please write form</small>}
+                    <label className="form-label">Composition</label>
+                    <input type="text" {...register("composition", { required: true })} className={`form-control ${errors.composition && "border-danger"}`} id="composition" />
+                    {errors.composition && <small className="text-danger">Please write composition</small>}
                   </div>
                 </div>
+
                 <div className="row">
                   <div className="mb-3 col-md-6">
                     <label className="form-label">Manufacturer</label>
@@ -73,34 +97,48 @@ const AddMedicine = () => {
                     {errors.manufacturer && <small className="text-danger">Please write manufacturer</small>}
                   </div>
                   <div className="mb-3 col-md-6">
-                    <label className="form-label">Unit Price</label>
-                    <input type="number" {...register("price", { required: true, min: 1 })} className={`form-control ${errors.price && "border-danger"}`} id="price" />
+                    <label className="form-label">Form</label>
+                    <input type="text" {...register("form", { required: true })} className={`form-control ${errors.form && "border-danger"}`} id="form" />
+                    {errors.form && <small className="text-danger">Please write form</small>}
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="mb-3 col-md-6">
+                    <label className="form-label">Price</label>
+                    <input type="number" {...register("price", { required: true })} className={`form-control ${errors.price && "border-danger"}`} id="price" />
                     {errors.price && <small className="text-danger">Please write price</small>}
+                  </div>
+                  <div className="mb-3 col-md-6">
+                    <label className="form-label">Unit Price</label>
+                    <input type="number" {...register("unitPrice", { required: true, min: 1 })} className={`form-control ${errors.unitPrice && "border-danger"}`} id="unitPrice" />
+                    {errors.unitPrice && <small className="text-danger">Please write unit price</small>}
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="mb-3 col-md-6">
+                    <label className="form-label">Quantity</label>
+                    <input type="number" {...register("quantity", { required: true })} className={`form-control ${errors.quantity && "border-danger"}`} id="quantity" />
+                    {errors.quantity && <small className="text-danger">Please write quantity</small>}
+                  </div>
+                  <div className="mb-3 col-md-6">
+                    <label className="form-label">Dose</label>
+                    <input type="text" {...register("dose", { required: true })} className={`form-control ${errors.dose && "border-danger"}`} id="dose" />
+                    {errors.dose && <small className="text-danger">Please write dose</small>}
                   </div>
                 </div>
 
                 <div className="row">
                   <div className="mb-3 col-md-6">
                     <label className="form-label">Pack Size</label>
-                    <input type="text" {...register("pack_size", { required: true })} className={`form-control ${errors.pack_size && "border-danger"}`} id="pack_size" />
-                    {errors.pack_size && <small className="text-danger">Please write pack size</small>}
+                    <input type="text" {...register("packSize", { required: true })} className={`form-control ${errors.packSize && "border-danger"}`} id="packSize" />
+                    {errors.packSize && <small className="text-danger">Please write pack size</small>}
                   </div>
                   <div className="mb-3 col-md-6">
                     <label className="form-label">Withdrawal Period</label>
-                    <input type="text" {...register("withdrawal_period", { required: true })} className={`form-control ${errors.withdrawal_period && "border-danger"}`} id="withdrawal_period" />
-                    {errors.withdrawal_period && <small className="text-danger">Please write withdrawal period</small>}
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="mb-3 col-md-6">
-                    <label className="form-label">Dose</label>
-                    <input type="text" {...register("dose", { required: true })} className={`form-control ${errors.dose && "border-danger"}`} id="dose" />
-                    {errors.dose && <small className="text-danger">Please write dose</small>}
-                  </div>
-                  <div className="mb-3 col-md-6">
-                    <label className="form-label">Route</label>
-                    <input type="text" {...register("route", { required: true })} className={`form-control ${errors.route && "border-danger"}`} id="route" />
-                    {errors.route && <small className="text-danger">Please write route</small>}
+                    <input type="text" {...register("withdrawalPeriod", { required: true })} className={`form-control ${errors.withdrawalPeriod && "border-danger"}`} id="withdrawalPeriod" />
+                    {errors.withdrawalPeriod && <small className="text-danger">Please write withdrawal period</small>}
                   </div>
                 </div>
 
@@ -111,9 +149,17 @@ const AddMedicine = () => {
                     {errors.strength && <small className="text-danger">Please write strength</small>}
                   </div>
                   <div className="mb-3 col-md-6">
+                    <label className="form-label">Route</label>
+                    <input type="text" {...register("route", { required: true })} className={`form-control ${errors.route && "border-danger"}`} id="route" />
+                    {errors.route && <small className="text-danger">Please write route</small>}
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="mb-3 col-md-6">
                     <label className="form-label">Animal Type</label>
                     {/* select field */}
-                    <select {...register("animal_type", { required: true })} className={`form-select ${errors.animal_type && "border-danger"}`} aria-label="Default select example">
+                    <select {...register("animalType", { required: true })} className={`form-select ${errors.animalType && "border-danger"}`} aria-label="Default select example">
                       <option selected value="">
                         Select
                       </option>
@@ -121,11 +167,19 @@ const AddMedicine = () => {
                       <option value="two">Two</option>
                       <option value="three">Three</option>
                     </select>
-                    {errors.animal_type && <small className="text-danger">Please select any animal type</small>}
+                    {errors.animalType && <small className="text-danger">Please select any animal type</small>}
+                  </div>
+                  <div className="mb-3 col-md-6">
+                    <label className="form-label">Description</label>
+                    <textarea type="text" {...register("description", { required: true })} className={`form-control ${errors.description && "border-danger"}`} id="description" />
+                    {errors.description && <small className="text-danger">Please write description</small>}
                   </div>
                 </div>
 
-                <div className="mb-3">
+                <div className="my-3 d-flex justify-content-center gap-4">
+                  <button type="reset" className="btn btn-danger text-white">
+                    Reset
+                  </button>
                   <button type="submit" className="btn app-btn-primary">
                     Add Medicine
                   </button>
--- Components/Medicine/ViewMedicine.js
+++ Components/Medicine/ViewMedicine.js
@@ -1,36 +1,107 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Swal from "sweetalert2";
+import { deleteMedicineData, fetchMedicine } from "../../features/medicine/medicineSlice";
+import Loader from "../UI/Loader";
 
 const ViewMedicine = () => {
+  const dispatch = useDispatch();
+  const { medicines, status } = useSelector((state) => state.medicine);
+
+  useEffect(() => {
+    dispatch(fetchMedicine());
+  }, [dispatch]);
+
+  // handling delete single medicine
+  const handleDeleteMedicine = async (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#15a362",
+      cancelButtonColor: "#ef4444",
+      confirmButtonText: "Yes, delete it!",
+      color: "#eaeaea",
+      background: "#161719",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const response = await dispatch(deleteMedicineData(id));
+
+          if (response?.payload?.success) {
+            dispatch(fetchMedicine());
+
+            Swal.fire({
+              icon: "success",
+              title: "Deleted!",
+              text: "Medicine has been deleted.",
+              showConfirmButton: false,
+              timer: 1500,
+              color: "#eaeaea",
+              background: "#161719",
+            });
+          } else {
+            Swal.fire({
+              icon: "error",
+              title: "Error!",
+              text: "Failed to delete medicine. Please try again later.",
+              confirmButtonColor: "#15a362",
+              color: "#eaeaea",
+              background: "#161719",
+            });
+          }
+        } catch (error) {
+          Swal.fire({
+            icon: "error",
+            title: "Something is wrong",
+            text: error,
+            color: "#eaeaea",
+            background: "#161719",
+          });
+        }
+      }
+    });
+  };
+
   return (
     <div className="container-fluid">
       <div className="app-card p-5 text-center shadow-sm mt-5">
         <div className="d-flex justify-content-between">
           <h3 className="page-title mb-4">Medicines</h3>
-          <div>{/* <button data-bs-toggle="modal" data-bs-target="#addUser" className="btn app-btn-primary">Add User</button> */}</div>
         </div>
         <div className="mb-4">
-          <table className="table table-dark">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Brand Name</th>
-                <th>Withdrawal Period</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Medicine 1</td>
-                <td>Brand Name 1</td>
-                <td>Withdrawal Period 1</td>
-                <td>
-                  <button className="btn app-btn-primary mb-2">Edit</button>
-                  <button className="btn app-btn-primary mx-2 mb-2">Delete</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="table-responsive">
+            <table className="table table-hover table-borderless table-striped table-dark">
+              <thead>
+                <tr>
+                  <th>SL.No</th>
+                  <th>Medicine Name</th>
+                  <th>Brand Name</th>
+                  <th>Withdrawal Period</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {medicines?.data?.map((medicine, idx) => (
+                  <tr key={medicine._id}>
+                    <td>{idx + 1}</td>
+                    <td className="text-nowrap">{medicine.name}</td>
+                    <td className="">{medicine.brandName}</td>
+                    <td className="">{medicine.withdrawalPeriod}</td>
+                    <td className="d-flex gap-3 justify-content-center">
+                      <button className="btn btn-info text-white">Edit</button>
+                      <button onClick={() => handleDeleteMedicine(medicine._id)} className="btn btn-danger text-white">
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
+        {status === "loading" && <Loader />}
       </div>
     </div>
   );
