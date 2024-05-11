57de991a30677c55f34d7847cb03bb393edf0a58
atikur-24 <atikurrahman7223@gmail.com>
2024-05-11 22:06:25 +0600

    fixed loader class name

--- Components/UI/Loader.jsx
+++ Components/UI/Loader.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Loader = () => {
   return (
     <div className="d-flex justify-content-center align-items-center h-100 pt-5">
-      <div class="spinner-border text-success" role="status">
-        <span class="visually-hidden">Loading...</span>
+      <div className="spinner-border text-success" role="status">
+        <span className="visually-hidden">Loading...</span>
       </div>
     </div>
   );
