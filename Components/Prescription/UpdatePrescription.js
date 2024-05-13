86a5a2d3148d37213a7ef862f2fc9157d89b29cf
atikur-24 <atikurrahman7223@gmail.com>
2024-05-13 12:56:28 +0600

    update prescription data

--- Components/Prescription/UpdatePrescription.js
+++ Components/Prescription/UpdatePrescription.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 import { Controller, useForm } from "react-hook-form";
-import toast from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
 import Select from "react-select";
-import { fetchAppointmentsByPhone } from "../../features/appointment/appointmentSlice";
 import { fetchMedicine } from "../../features/medicine/medicineSlice";
-import { createPrescription } from "../../features/prescription/prescriptionSlice";
+import { fetchSinglePrescription } from "../../features/prescription/prescriptionSlice";
 import { fetchTest } from "../../features/test/testSlice";
+import Loader from "../UI/Loader";
 
 // Define custom styles
 const customStyles = {
@@ -37,66 +37,23 @@ const customStyles = {
   }),
 };
 
-const PrescriptionHome = () => {
-  const [searchPhone, setSearchPhone] = useState("");
-  const [patentInfo, setPatentInfo] = useState([]);
-  const [selectedPatentInfo, setSelectedPatentInfo] = useState({});
+const UpdatePrescription = () => {
+  const router = useRouter();
+  const { id } = router.query;
   const dispatch = useDispatch();
+  const { prescription, status } = useSelector((state) => state.prescription);
   const { tests } = useSelector((state) => state.test);
   const { medicines } = useSelector((state) => state.medicine);
+  console.log(prescription?.data);
 
-  const { handleSubmit, register, control, reset } = useForm();
+  const { handleSubmit, register, control } = useForm({ values: prescription?.data });
 
-  const getPatentByPhone = async () => {
-    try {
-      if (searchPhone === "") return;
-
-      const res = await dispatch(fetchAppointmentsByPhone(searchPhone));
-      const existingPatentData = res?.payload?.data;
-
-      if (existingPatentData.length > 0) {
-        setPatentInfo(existingPatentData);
-        toast.success("Patent's Data Found");
-      } else {
-        toast.error("No Data Found!");
-        setSelectedPatentInfo({});
-        setPatentInfo([]);
-        reset();
-      }
-    } catch (error) {
-      toast.error("No Data Found!");
-      console.error(error);
-    }
-  };
-
-  const getPatentInfo = (id) => {
-    const selectedPatent = patentInfo?.find((patent) => patent._id === id);
-    setSelectedPatentInfo(selectedPatent);
-  };
-
-  const onSubmit = async (prescriptionData) => {
-    try {
-      prescriptionData.medicines = prescriptionData?.medicines?.map((medicine) => medicine.value);
-      prescriptionData.tests = prescriptionData?.tests?.map((test) => test.value);
-
-      const response = await dispatch(createPrescription(prescriptionData));
-
-      if (response?.payload?.success) {
-        toast.success("Prescription added successfully!");
-        setSearchPhone("");
-        setSelectedPatentInfo({});
-        setPatentInfo([]);
-        reset();
-      } else {
-        toast.error("Failed to add prescription! Please try again later.");
-      }
-    } catch (error) {
-      toast.error("An error occurred while adding prescription. Please try again later.");
-      console.error(error);
-    }
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   useEffect(() => {
+    dispatch(fetchSinglePrescription(id));
     dispatch(fetchMedicine());
     dispatch(fetchTest());
   }, [dispatch]);
@@ -112,76 +69,57 @@ const PrescriptionHome = () => {
     label: medicine.name,
   }));
 
+  //   loader
+  if (status === "loading") return <Loader />;
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-12">
           <div className="card">
             <div className="card-header">
-              <h4 className="card-header-title text-center">Prescription</h4>
+              <h4 className="card-header-title text-center">Edit Prescription</h4>
             </div>
             <div className="card-body">
-              <div className="row mb-4">
-                <div className="col-md-6">
-                  <input
-                    onChange={(e) => {
-                      setSearchPhone(e.target.value);
-                    }}
-                    type="text"
-                    className="form-control"
-                    placeholder="Patent's Phone"
-                    aria-label="Patent's phone"
-                    aria-describedby="button-addon2"
-                  />
-                  <button onClick={getPatentByPhone} className="btn my-2 mx-1 btn-primary text-white" type="button" id="button-addon2">
-                    Search
-                  </button>
-                  <span className="small opacity-75 ps-2">(Search patent's phone for prescription)</span>
-                </div>
-                <div className="col-md-6"></div>
-              </div>
               <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="row">
                   <div className="mb-3 col-md-6">
-                    <label className="form-label">Select Appointment</label>
+                    <label className="form-label">Selected Appointment</label>
                     {/* show case no, owner name & date */}
-                    <select {...register("appointment", { required: true })} onChange={(e) => getPatentInfo(e.target.value)} className="form-select" aria-label="Default select example">
-                      <option value="">Select</option>
-                      {patentInfo?.map((patent) => (
-                        <option key={patent._id} value={patent._id}>
-                          {patent.caseNo} {patent.ownerName} {patent.date}
-                        </option>
-                      ))}
+                    <select {...register("appointment", { required: true })} className="form-select" aria-label="Default select example">
+                      <option selected>
+                        {prescription?.data?.appointment?.caseNo} {prescription?.data?.appointment?.ownerName} {prescription?.data?.appointment?.date}
+                      </option>
                     </select>
                   </div>
                   <div className="mb-3 col-md-6">
                     <label className="form-label">CASE NO</label>
-                    <input type="number" readOnly required value={selectedPatentInfo?.caseNo} className="form-control" />
+                    <input type="number" readOnly required value={prescription?.data?.appointment?.caseNo} className="form-control" />
                   </div>
                 </div>
                 <div className="row">
                   <div className="mb-3 col-md-6">
                     <label className="form-label">Owner Name</label>
-                    <input type="text" readOnly required value={selectedPatentInfo?.ownerName} className="form-control" />
+                    <input type="text" readOnly required value={prescription?.data?.appointment?.ownerName} className="form-control" />
                   </div>
                   <div className="mb-3 col-md-6">
                     <label className="form-label">Phone</label>
-                    <input type="text" readOnly required value={selectedPatentInfo?.phone} className="form-control" />
+                    <input type="text" readOnly required value={prescription?.data?.appointment?.phone} className="form-control" />
                   </div>
                 </div>
                 <div className="row">
                   <div className="mb-3 col-md-6">
                     <label className="form-label">District</label>
-                    <input type="text" readOnly required value={selectedPatentInfo?.district} className="form-control" />
+                    <input type="text" readOnly required value={prescription?.data?.appointment?.district} className="form-control" />
                   </div>
                   <div className="mb-3 col-md-6">
                     <label className="form-label">Upazila</label>
-                    <input type="text" readOnly required value={selectedPatentInfo?.upazila} className="form-control" />
+                    <input type="text" readOnly required value={prescription?.data?.appointment?.upazila} className="form-control" />
                   </div>
                 </div>
                 <div className="mb-3">
                   <label className="form-label">Address</label>
-                  <input readOnly required value={selectedPatentInfo?.address} className="form-control"></input>
+                  <input readOnly required value={prescription?.data?.appointment?.address} className="form-control"></input>
                 </div>
                 <div className="row">
                   <div className="mb-3">
@@ -211,12 +149,9 @@ const PrescriptionHome = () => {
                     <textarea {...register("advice")} className="form-control" rows="5"></textarea>
                   </div>
                 </div>
-                <div className="my-3 d-flex justify-content-center gap-4">
-                  <button type="reset" className="btn btn-danger text-white">
-                    Reset
-                  </button>
+                <div className="my-3 d-flex justify-content-center">
                   <button type="submit" className="btn app-btn-primary">
-                    Submit
+                    Update Prescription
                   </button>
                 </div>
               </form>
@@ -228,4 +163,4 @@ const PrescriptionHome = () => {
   );
 };
 
-export default PrescriptionHome;
+export default UpdatePrescription;
---
+++ src/pages/prescription/[id]/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UpdatePrescription from "../../../../Components/Prescription/UpdatePrescription";
+
+const index = () => {
+  return (
+    <div>
+      <UpdatePrescription />
+    </div>
+  );
+};
+
+export default index;
