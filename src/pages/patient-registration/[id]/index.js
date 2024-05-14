3ecf8b9881429ebfc5681d5f75722175e6c1a17b
atikur-24 <atikurrahman7223@gmail.com>
2024-05-14 14:17:32 +0600

    update patient registration data

---
+++ src/pages/patient-registration/[id]/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UpdatePatientRegistration from "../../../../Components/PatientRegistration/UpdatePatientRegistration";
+
+const index = () => {
+  return (
+    <div>
+      <UpdatePatientRegistration />
+    </div>
+  );
+};
+
+export default index;
