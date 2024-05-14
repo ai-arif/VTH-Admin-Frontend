742393389734f1a5d20a01f674b7bc10ebaf8c3d
atikur-24 <atikurrahman7223@gmail.com>
2024-05-15 01:03:42 +0600

    changes input filed

--- Components/Users/modals/AddUser.js
+++ Components/Users/modals/AddUser.js
@@ -1,43 +1,56 @@
 import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchDepartment } from "../../../features/department/departmentSlice";
-import { createUserAsync } from "../../../features/user/userSlice";
 
 const AddUser = () => {
+  const [department, setDepartment] = useState("");
+
   const dispatch = useDispatch();
-  const [loading, setLoading] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [isDoctor, setIsDoctor] = useState(false);
   const { departments } = useSelector((state) => state.department);
-  const [userObj, setUserObj] = useState({ fullName: "", phone: "", password: "", role: "" });
+
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  console.log(department);
 
   const handleChange = (e) => {
-    if (e.target.value === "doctor") {
-      setIsDoctor(true);
-    } else {
-      setIsDoctor(false);
-    }
-    setUserObj({ ...userObj, [e.target.name]: e.target.value });
+    console.log(e);
+    // if (e.target.value === "doctor") {
+    //   setIsDoctor(true);
+    // } else {
+    //   setIsDoctor(false);
+    // }
   };
+
   // password show hide toggle
   const handleTogglePassword = () => {
     setShowPassword(!showPassword);
   };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    try {
-      console.log(userObj);
-      await dispatch(createUserAsync(userObj));
-      setLoading(false);
-      setUserObj({ fullName: "", phone: "", password: "", role: "" });
-    } catch (error) {
-      console.log(error);
-      setLoading(false);
-    }
-    // close modal
-    document.getElementById("closeModal").click();
+
+  const onSubmit = async (staffData) => {
+    console.log(staffData);
+    // try {
+    //   const response = await dispatch(createDepartment(department));
+
+    //   if (response?.payload?.success) {
+    //     toast.success("Department added successfully!");
+    //     reset();
+    //     document.getElementById("closeModal").click();
+    //   } else {
+    //     toast.error("Failed to add department! Please try again later.");
+    //   }
+    // } catch (error) {
+    //   console.error("An error occurred while adding department:", error);
+    //   toast.error("An error occurred while adding department. Please try again later.");
+    // }
   };
 
   useEffect(() => {
@@ -56,24 +69,28 @@ const AddUser = () => {
               <button id="closeModal" type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
-              <form>
+              <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-3">
                   <label htmlFor="fullName" className="form-label">
                     Full Name
                   </label>
-                  <input value={userObj.fullName} onChange={handleChange} name="fullName" type="text" className="form-control" id="fullName" aria-describedby="emailHelp" />
+                  <input type="text" {...register("fullName", { required: true })} className={`form-control ${errors.fullName && "border-danger"}`} />
+                  {errors.fullName && <small className="text-danger">Please write full name</small>}
                 </div>
                 <div className="mb-3">
                   <label htmlFor="phone" className="form-label">
                     Phone
                   </label>
-                  <input value={userObj.phone} onChange={handleChange} name="phone" type="text" className="form-control" id="phone" />
+                  <input type="text" {...register("phone", { required: true })} className={`form-control ${errors.phone && "border-danger"}`} />
+                  {errors.phone && <small className="text-danger">Please write phone</small>}
                 </div>
                 <div className="mb-3 position-relative">
                   <label htmlFor="password" className="form-label">
                     Password
                   </label>
-                  <input value={userObj.password} onChange={handleChange} name="password" type={showPassword ? "text" : "password"} className="form-control" id="password" />
+                  <input type={showPassword ? "text" : "password"} {...register("password", { required: true })} className={`form-control ${errors.password && "border-danger"}`} />
+                  {errors.password && <small className="text-danger">Please write password</small>}
+
                   <div onClick={handleTogglePassword} type="button" className="position-absolute" id="user-eye">
                     {showPassword ? <AiFillEye size={18} /> : <AiFillEyeInvisible size={18} />}
                   </div>
@@ -82,18 +99,21 @@ const AddUser = () => {
                   <label htmlFor="role" className="form-label">
                     Role
                   </label>
-                  <select value={userObj.role} onChange={handleChange} name="role" className="form-select" id="role">
-                    <option>Open this select menu</option>
+                  <select onChange={(e) => handleChange(e)} {...register("role", { required: true })} className={`form-select ${errors.role && "border-danger"}`} aria-label="Default select example">
+                    <option value="">Open this select menu</option>
                     <option value="admin">Admin</option>
                     <option value="doctor">Doctor</option>
                     <option value="lab">Lab</option>
-                    <option value="pharmacy">Pharmary</option>
+                    <option value="pharmacy">Pharmacy</option>
                   </select>
+                  {errors.role && <small className="text-danger">Please select any role</small>}
                 </div>
                 {isDoctor && (
                   <div className="mb-3">
-                    <label className="form-label">Department</label>
-                    <select name="department" className="form-select" aria-label="Default select example">
+                    <label htmlFor="department" className="form-label">
+                      Department
+                    </label>
+                    <select {...register("department", { required: true })} className={`form-select ${errors.department && "border-danger"}`} aria-label="Default select example">
                       <option value="">Select</option>
                       {departments?.map((department) => (
                         <option key={department._id} value={department._id}>
@@ -101,22 +121,16 @@ const AddUser = () => {
                         </option>
                       ))}
                     </select>
+                    {errors.department && <small className="text-danger">Please select any department</small>}
                   </div>
                 )}
                 <div className="d-flex gap-4 justify-content-end">
                   <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
                     Close
                   </button>
-                  {loading ? (
-                    <button className="btn app-btn-primary" type="button" disabled>
-                      <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                      Loading...
-                    </button>
-                  ) : (
-                    <button onClick={handleSubmit} type="submit" className="btn app-btn-primary">
-                      Submit
-                    </button>
-                  )}
+                  <button type="submit" className="btn app-btn-primary">
+                    Submit
+                  </button>
                 </div>
               </form>
             </div>
