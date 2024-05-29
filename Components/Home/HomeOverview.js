a1c5288ea530dd4bc5c71d4ae769d8cfc8bc3b46
atikur-24 <atikurrahman7223@gmail.com>
2024-05-29 11:36:59 +0600

    create a home overview page

---
+++ Components/Home/HomeOverview.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomeOverview = () => {
+  return (
+    <div>
+      <h1 className="text-center my-4">This is Overview page...</h1>
+    </div>
+  );
+};
+
+export default HomeOverview;
