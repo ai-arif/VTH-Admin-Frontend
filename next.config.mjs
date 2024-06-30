f34f8a6bcbbddf2a2749ee40495f10aa95d27515
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-06-30 23:49:05 +0600

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     API_URL: "https://api.bauvth.com/api/v1",
+    // API_URL: "http://localhost:5000/api/v1",
   },
   images: {
     domains: ["storage.googleapis.com"],
