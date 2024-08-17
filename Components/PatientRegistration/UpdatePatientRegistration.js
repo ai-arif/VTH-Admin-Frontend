1de90d5c5e1ef805cb54b7ee3ac6565e80efe4b4
atikur-24 <atikurrahman7223@gmail.com>
2024-08-17 13:42:18 +0600

    middlware is off

--- Components/PatientRegistration/UpdatePatientRegistration.js
+++ Components/PatientRegistration/UpdatePatientRegistration.js
@@ -329,7 +329,7 @@ const UpdatePatientRegistration = () => {
                           {errors.totalSickAnimals && <small className="text-danger">Please write total sick animals</small>}
                         </div>
                         <div className="mb-3 col-md-6">
-                          <label className="form-label">Mortality (%)</label>
+                          <label className="form-label">Morbidity (%)</label>
                           <Controller
                             name="totalMortality"
                             control={control}
